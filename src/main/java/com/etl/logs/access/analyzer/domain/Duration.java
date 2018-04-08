7631cac8ed8d0a0735693762a78626f436930d6f
fmachi <filippomachi@gmail.com>
2018-04-08 22:02:08 +0200

    Added Duration enum

--- src/main/java/com/etl/logs/access/analyzer/domain/Duration.java
+++ src/main/java/com/etl/logs/access/analyzer/domain/Duration.java
@@ -1,4 +1,14 @@
 package com.etl.logs.access.analyzer.domain;
 
 public enum Duration {
+    HOURLY,
+    DAILY;
+
+    public boolean isDaily() {
+        return DAILY.equals(this);
+    }
+
+    public boolean isHourly() {
+        return HOURLY.equals(this);
+    }
 }
