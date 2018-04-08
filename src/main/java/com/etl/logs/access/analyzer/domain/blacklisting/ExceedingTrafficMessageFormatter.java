1bf9e9628debcbb3289b2b1ce1bab6148de8b17d
fmachi <filippomachi@gmail.com>
2018-04-08 23:27:18 +0200

    Added message formatter

--- src/main/java/com/etl/logs/access/analyzer/domain/blacklisting/ExceedingTrafficMessageFormatter.java
+++ src/main/java/com/etl/logs/access/analyzer/domain/blacklisting/ExceedingTrafficMessageFormatter.java
@@ -3,15 +3,22 @@ package com.etl.logs.access.analyzer.domain.blacklisting;
 import com.etl.logs.access.analyzer.domain.accesslog.ExceedingTrafficIp;
 import com.etl.logs.access.analyzer.port.persistence.accesslog.ExceedingTrafficCriteria;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.function.BiFunction;
 
-public class MessageFormatter implements BiFunction<ExceedingTrafficIp,ExceedingTrafficCriteria,String>{
+public class ExceedingTrafficMessageFormatter implements BiFunction<ExceedingTrafficIp, ExceedingTrafficCriteria, String> {
     @Override
     public String apply(ExceedingTrafficIp ipsToBlacklist, ExceedingTrafficCriteria exceedingTrafficCriteria) {
-        return String.format("The ip '%s' has been blocked because it performed %d access from '%t' to '%t'.",
+        return String.format("The ip '%s' has been blocked because it performed %d access from '%s' to '%s'.",
                 ipsToBlacklist.getIpAddress(),
                 ipsToBlacklist.getNumberOfaccess(),
-                exceedingTrafficCriteria.getLowerBound(),
-                exceedingTrafficCriteria.getUpperBound());
+                toString(exceedingTrafficCriteria.getLowerBound()),
+                toString(exceedingTrafficCriteria.getUpperBound())
+        );
+    }
+
+    private Object toString(LocalDateTime value) {
+        return Optional.ofNullable(value).map(LocalDateTime::toString).orElse("null");
     }
 }
--- src/test/java/com/etl/logs/access/analyzer/domain/blacklisting/ExceedingTrafficMessageFormatterTest.java
+++ src/test/java/com/etl/logs/access/analyzer/domain/blacklisting/ExceedingTrafficMessageFormatterTest.java
@@ -1,5 +1,32 @@
+package com.etl.logs.access.analyzer.domain.blacklisting;
+
+import com.etl.logs.access.analyzer.domain.accesslog.ExceedingTrafficIp;
+import com.etl.logs.access.analyzer.port.persistence.accesslog.ExceedingTrafficCriteria;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
 public class ExceedingTrafficMessageFormatterTest {
+
+    ExceedingTrafficMessageFormatter formatter = new ExceedingTrafficMessageFormatter();
+
+    @Test
+    public void shouldFormatExceedingTrafficMessage() {
+
+        ExceedingTrafficIp ipToBlacklist = ExceedingTrafficIp.builder()
+                .numberOfaccess(200)
+                .ipAddress("192.168.25.61")
+                .build();
+        ExceedingTrafficCriteria trafficCriteria = ExceedingTrafficCriteria.builder()
+                .lowerBound(LocalDateTime.of(2018,1,1,13,12,25))
+                .upperBound(LocalDateTime.of(2018,1,1,14,12,25))
+                .build();
+
+        String formatMessage = formatter.apply(ipToBlacklist, trafficCriteria);
+
+        assertEquals("The ip '192.168.25.61' has been blocked because it performed 200 access from '2018-01-01T13:12:25' to '2018-01-01T14:12:25'.",formatMessage);
+    }
 
 }
