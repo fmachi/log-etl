34811769035f020f026a2043b66e79e521103582
fmachi <filippomachi@gmail.com>
2018-04-08 22:04:43 +0200

    Add application launcher

--- src/main/java/com/etl/logs/access/analyzer/ApplicationLauncher.java
+++ src/main/java/com/etl/logs/access/analyzer/ApplicationLauncher.java
@@ -1,18 +1,27 @@
-package com.zopa.exercise.ratecalculator;
+package com.etl.logs.access.analyzer;
 
-import com.zopa.exercise.ratecalculator.application.LoanQuoteApplication;
+import com.etl.logs.access.analyzer.domain.ui.InputParameters;
+import com.etl.logs.access.analyzer.domain.usecase.LogAccessCheckerUseCase;
+import com.etl.logs.access.analyzer.port.ui.InputParametersParser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "com.zopa.exercise.ratecalculator")
+@SpringBootApplication(scanBasePackages = "com.etl.logs.access.analyzer")
 @Import(ApplicationConfiguration.class)
+@Slf4j
 public class ApplicationLauncher implements CommandLineRunner {
 
+
     @Autowired
-    LoanQuoteApplication loanQuoteApplication;
+    InputParametersParser inputParametersParser;
+
+    @Autowired
+    LogAccessCheckerUseCase logAccessCheckerUseCase;
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ApplicationLauncher.class, args);
@@ -20,6 +29,11 @@ public class ApplicationLauncher implements CommandLineRunner {
 
     @Override
     public void run(String... params) throws Exception {
-        loanQuoteApplication.run(params);
+        inputParametersParser.parse(params).ifPresent(this::processAccessLogFile);
+    }
+
+
+    private void processAccessLogFile(InputParameters inputParameters) {
+        logAccessCheckerUseCase.process(inputParameters);
     }
 }
