e54fc64f9a3f7ea09a37676e170e0c8c9fd03947
fmachi <filippomachi@gmail.com>
2018-04-08 22:03:36 +0200

    Added class to remove database lines

--- src/main/java/com/etl/logs/access/analyzer/domain/persistence/DatabaseCleaner.java
+++ src/main/java/com/etl/logs/access/analyzer/domain/persistence/DatabaseCleaner.java
@@ -1,4 +1,21 @@
 package com.etl.logs.access.analyzer.domain.persistence;
 
+import com.etl.logs.access.analyzer.port.persistence.accesslog.LogAccessRepository;
+import com.etl.logs.access.analyzer.port.persistence.blacklisting.BlacklistingRepository;
+
 public class DatabaseCleaner {
+
+    private final LogAccessRepository logAccessRepository;
+    private final BlacklistingRepository blacklistingRepository;
+
+    public DatabaseCleaner(LogAccessRepository logAccessRepository, BlacklistingRepository blacklistingRepository) {
+        this.logAccessRepository = logAccessRepository;
+        this.blacklistingRepository = blacklistingRepository;
+    }
+
+    public void cleanDatabase() {
+        logAccessRepository.cleanPreviousElaboration();
+
+        blacklistingRepository.cleanPreviousElaboration();
+    }
 }
