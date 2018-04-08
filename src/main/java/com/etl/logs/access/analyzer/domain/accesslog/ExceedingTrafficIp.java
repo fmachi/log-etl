f105b30189606f6a721155d111f4154b9b445509
fmachi <filippomachi@gmail.com>
2018-04-08 22:02:43 +0200

    Added bean to rapresent ip that exceeded the threshold

--- src/main/java/com/etl/logs/access/analyzer/domain/accesslog/ExceedingTrafficIp.java
+++ src/main/java/com/etl/logs/access/analyzer/domain/accesslog/ExceedingTrafficIp.java
@@ -1,4 +1,13 @@
-package com.etl.logs.access.analyzer.domain;
+package com.etl.logs.access.analyzer.domain.accesslog;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
 public class ExceedingTrafficIp {
+    String ipAddress;
+    int numberOfaccess;
 }
