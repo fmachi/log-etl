f5113d7aa0aaa7246c348ad788a014fc418cfca2
fmachi <filippomachi@gmail.com>
2018-04-08 22:03:15 +0200

    Added bean to rapresent criteria to match ip exceeding traffic quote

--- src/main/java/com/etl/logs/access/analyzer/port/persistence/accesslog/ExceedingTrafficCriteria.java
+++ src/main/java/com/etl/logs/access/analyzer/port/persistence/accesslog/ExceedingTrafficCriteria.java
@@ -1,4 +1,14 @@
-package com.etl.logs.access.analyzer.port.persistence;
+package com.etl.logs.access.analyzer.port.persistence.accesslog;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
 public class ExceedingTrafficCriteria {
+    LocalDateTime lowerBound;
+    LocalDateTime upperBound;
+    int maxNumberOfAccess;
 }
