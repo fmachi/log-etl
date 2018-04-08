23cf591344506dca3c9d8b0bffe6aff799d8c0f6
fmachi <filippomachi@gmail.com>
2018-04-08 22:04:27 +0200

    Added application configuration

--- src/main/java/com/etl/logs/access/analyzer/ApplicationConfiguration.java
+++ src/main/java/com/etl/logs/access/analyzer/ApplicationConfiguration.java
@@ -1,92 +1,82 @@
-package com.zopa.exercise.ratecalculator;
+package com.etl.logs.access.analyzer;
 
-import com.zopa.exercise.ratecalculator.application.CSVBasedRateCalculationSystemFactory;
-import com.zopa.exercise.ratecalculator.application.LoanQuoteApplication;
-import com.zopa.exercise.ratecalculator.engine.MortgageCalculator;
-import com.zopa.exercise.ratecalculator.persistence.validation.CSVLineValidator;
-import com.zopa.exercise.ratecalculator.presentation.Console;
-import com.zopa.exercise.ratecalculator.presentation.STDOUTConsole;
-import com.zopa.exercise.ratecalculator.validation.AmountValidator;
-import com.zopa.exercise.ratecalculator.validation.FileNameValidator;
-import com.zopa.exercise.ratecalculator.validation.InputParametersValidator;
-import org.springframework.beans.factory.annotation.Value;
+import com.etl.logs.access.analyzer.adapter.ui.BlacklistedIpPrinter;
+import com.etl.logs.access.analyzer.adapter.ui.CLIInputParametersParser;
+import com.etl.logs.access.analyzer.adapter.persistence.accesslog.JdbcTemplateLogAccessRepository;
+import com.etl.logs.access.analyzer.adapter.io.BufferedReaderLogLinesSource;
+import com.etl.logs.access.analyzer.adapter.persistence.blacklisting.JdbcTemplateBlacklistingRepository;
+import com.etl.logs.access.analyzer.domain.blacklisting.BlacklistChecker;
+import com.etl.logs.access.analyzer.domain.blacklisting.BlacklistedIp;
+import com.etl.logs.access.analyzer.domain.feeder.AccessLogFeeder;
+import com.etl.logs.access.analyzer.domain.feeder.PipelineConfiguration;
+import com.etl.logs.access.analyzer.domain.persistence.DatabaseCleaner;
+import com.etl.logs.access.analyzer.domain.usecase.LogAccessCheckerUseCase;
+import com.etl.logs.access.analyzer.port.io.LogLinesSource;
+import com.etl.logs.access.analyzer.port.persistence.accesslog.LogAccessRepository;
+import com.etl.logs.access.analyzer.port.persistence.blacklisting.BlacklistingRepository;
+import com.etl.logs.access.analyzer.port.ui.InputParametersParser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.function.Consumer;
 
 @Configuration
 public class ApplicationConfiguration {
 
-    @Value("${rate-calculator.engine.years}")
-    Integer years;
-
-    @Value("${rate-calculator.engine.compounding}")
-    Integer compounding;
-
     @Bean
-    LoanQuoteApplication loanQuoteApplication(InputParametersValidator validator, Console console, CSVBasedRateCalculationSystemFactory csvBasedRateCalculationSystemFactory) {
-        return new LoanQuoteApplication(
-                validator,
-                console,
-                csvBasedRateCalculationSystemFactory,
-                years,
-                compounding
-        );
+    public InputParametersParser inputParametersParser() {
+        return new CLIInputParametersParser();
     }
 
     @Bean
-    CSVBasedRateCalculationSystemFactory calculationSystemFactory(MortgageCalculator mortgageCalculator, CSVLineValidator csvLineValidator) {
-        return new CSVBasedRateCalculationSystemFactory(mortgageCalculator,csvLineValidator);
+    public LogAccessCheckerUseCase logAccessCheckerUseCase(DatabaseCleaner databaseCleaner, AccessLogFeeder accessLogFeeder, BlacklistChecker blacklistChecker) {
+        return new LogAccessCheckerUseCase(databaseCleaner, accessLogFeeder, blacklistChecker);
     }
 
     @Bean
-    CSVLineValidator csvLineValidator() {
-        return new CSVLineValidator();
+    public AccessLogFeeder accessLogFeeder(LogLinesSource logLinesSource, LogAccessRepository logAccessRepository, PipelineConfiguration pipelineConfiguration) {
+        return new AccessLogFeeder(logLinesSource, logAccessRepository, pipelineConfiguration);
     }
 
     @Bean
-    MortgageCalculator mortgageCalculator() {
-        return new MortgageCalculator();
-    }
-
-
-    @Bean
-    InputParametersValidator inputParametersValidator() {
-        return new InputParametersValidator(
-                new FileNameValidator(),
-                new AmountValidator()
-        );
+    PipelineConfiguration pipelineConfiguration() {
+        return PipelineConfiguration.builder()
+                .bufferSize(100)
+                .parallelism(4)
+                .prefetchSize(10)
+                .build();
     }
 
     @Bean
-    String noQuotationPossibleMessage() {
-        return "No quotation found";
+    BlacklistChecker blacklistChecker(LogAccessRepository logAccessRepository, BlacklistingRepository blacklistingRepository, Consumer<BlacklistedIp> blacklistedIpPrinter) {
+        return new BlacklistChecker(logAccessRepository, blacklistingRepository, blacklistedIpPrinter);
     }
 
     @Bean
-    String[] getInstructions() {
-        return new String[]{
-                "RateCalculationSystem Usage ",
-                "You can run this application with the following command: ",
-                "cmd> ratecalculator-1.0-SNAPSHOT.jar [market-file] [loan-amount]",
-                " if any offer is found, the output will be:",
-                "     Requested amount: £XXXX  ",
-                "     Rate: X.X%  ",
-                "     Monthly repayment: £XXXX.XX  ",
-                "     Total repayment: £XXXX.XX ",
-                " if no offer is found the application output will be: ",
-                "     No quotation possible"
-        };
+    DatabaseCleaner databaseCleaner(LogAccessRepository logAccessRepository, BlacklistingRepository blacklistingRepository) {
+        return new DatabaseCleaner(logAccessRepository, blacklistingRepository);
     }
 
     @Bean
-    STDOUTConsole console(String[] instructions,
-                          String noQuotationPossibleMessage) {
-        return new STDOUTConsole(
-                instructions,
-                noQuotationPossibleMessage,
-                System.out::println,
-                System.err::println
-        );
+    public LogLinesSource logLinesSource() {
+        return new BufferedReaderLogLinesSource("./logsample/access.log", "\\|");
     }
+
+    @Bean
+    public LogAccessRepository logAccessRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcTemplateLogAccessRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public BlacklistingRepository blacklistingRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcTemplateBlacklistingRepository(jdbcTemplate);
+    }
+
+    @Bean
+    BlacklistedIpPrinter blacklistedIpPrinter() {
+        return new BlacklistedIpPrinter();
+    }
+
 
 }
