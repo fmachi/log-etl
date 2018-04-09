84bb49f595f81c6009a66ff7a4855250f5b7b290
fmachi <filippomachi@gmail.com>
2018-04-09 22:31:33 +0200

    Added test for LogAccessCheckerUseCase

---
+++ src/test/java/com/etl/logs/access/analyzer/domain/usecase/LogAccessCheckerUseCaseTest.java
@@ -0,0 +1,45 @@
+package com.etl.logs.access.analyzer.domain.usecase;
+
+import com.etl.logs.access.analyzer.domain.blacklisting.BlacklistChecker;
+import com.etl.logs.access.analyzer.domain.feeder.AccessLogFeeder;
+import com.etl.logs.access.analyzer.domain.persistence.DatabaseCleaner;
+import com.etl.logs.access.analyzer.domain.ui.InputParameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LogAccessCheckerUseCaseTest {
+
+    LogAccessCheckerUseCase logAccessCheckerUseCase;
+    @Mock
+    private DatabaseCleaner databaseCleaner;
+    @Mock
+    private AccessLogFeeder accessLogFeeder;
+    @Mock
+    private BlacklistChecker blacklistChecker;
+
+    @Before
+    public void setup() {
+        logAccessCheckerUseCase = new LogAccessCheckerUseCase(databaseCleaner,accessLogFeeder,blacklistChecker);
+    }
+
+    @Test
+    public void shouldOrchestrateInnerPhashes() {
+        InputParameters inputParameters = InputParameters.builder().build();
+
+        logAccessCheckerUseCase.process(inputParameters);
+
+        verify(databaseCleaner).cleanDatabase();
+
+        verify(accessLogFeeder).feed();
+
+        verify(blacklistChecker).checkAndBlacklistIps(inputParameters);
+    }
+
+}
