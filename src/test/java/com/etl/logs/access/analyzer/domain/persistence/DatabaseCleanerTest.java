9664552dbf0ca1c32e491a6575c1ae88918975f9
Filippo Machi <filippo.machi@weekendesk.fr>
2018-04-09 17:47:49 +0200

    Added test for DatabaseCleaner

--- src/test/java/com/etl/logs/access/analyzer/domain/persistence/DatabaseCleanerTest.java
+++ src/test/java/com/etl/logs/access/analyzer/domain/persistence/DatabaseCleanerTest.java
@@ -1,5 +1,36 @@
-import static org.junit.Assert.*;
+package com.etl.logs.access.analyzer.domain.persistence;
 
+import com.etl.logs.access.analyzer.port.persistence.accesslog.LogAccessRepository;
+import com.etl.logs.access.analyzer.port.persistence.blacklisting.BlacklistingRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DatabaseCleanerTest {
+
+    DatabaseCleaner databaseCleaner;
+    @Mock
+    private LogAccessRepository logAccessRepository;
+    @Mock
+    private BlacklistingRepository blacklistingRepository;
+
+    @Before
+    public void setup() {
+        databaseCleaner = new DatabaseCleaner(logAccessRepository, blacklistingRepository);
+    }
+
+    @Test
+    public void shouldCleanupAllDatabases() {
+        databaseCleaner.cleanDatabase();
+
+        Mockito.verify(logAccessRepository).cleanPreviousElaboration();
+
+        Mockito.verify(blacklistingRepository).cleanPreviousElaboration();
+
+    }
 
 }
